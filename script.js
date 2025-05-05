d56450414efc40ff39119c67d53e69636f9c1311
Santiago Rodriguez <Srodriguezl.2601@gmail.com>
2025-05-05 18:59:59 -0400

    asegura el registro de service-worker

--- script.js
+++ script.js
@@ -312,3 +312,11 @@ function updateScoreLabel(value) {
     }
   }
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('✅ Service Worker registrado:', reg.scope))
+      .catch(err => console.error('❌ Error registrando el SW:', err));
+  });
+}
